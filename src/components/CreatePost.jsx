e6825d7d16d1dfe15f1ec43ef085093085b2516c
Ankit Mishra <ankitsocial1103@gmail.com>
2025-08-30 14:11:05 +0530

    fixed button errors

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import "react-awesome-button/dist/styles.css";
 import { FaLinkedin } from "react-icons/fa";
 
 const CreatePost = () => {
